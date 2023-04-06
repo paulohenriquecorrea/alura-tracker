ce2443e49fca52b49ac71a7a146892464f5f1f18
unknown <ph.sistemas.ti@gmail.com>
2023-04-06 09:52:55 -0400

    add rotas aninhadas

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import Tarefas from '../views/Tarefas.vue';
 import Projetos from '../views/Projetos.vue';
+import Formulario from '../views/Projetos/FormularioProjetos.vue';
 
 const rotas: RouteRecordRaw[] = [
   {
@@ -10,8 +11,27 @@ const rotas: RouteRecordRaw[] = [
   },
   {
     path: '/projetos',
-    name: 'Projetos',
     component: Projetos,
+    children: [
+      {
+        path: '',
+        name: 'Projetos',
+        component: Projetos,
+      },
+
+      {
+        path: '/novo',
+        name: 'Novo projeto',
+        component: Formulario,
+      },
+
+      {
+        path: '/:id',
+        name: 'Editar projeto',
+        component: Formulario,
+        props: true,
+      },
+    ],
   },
 ];
 
